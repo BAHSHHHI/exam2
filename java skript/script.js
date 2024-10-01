dc2f24f7450dda5762bae11f904e06062667610d
BAHSHHHI <abdirazakovsyimykbek2006@gmail.com>
2024-10-01 11:34:17 +0600

    Add files via upload

---
+++ java skript/script.js
@@ -0,0 +1,35 @@
+// console.log('start asdfasdf');
+// console.log(num)
+// console.log('str')
+// const bool = false 
+// // false
+// // true
+// console.log(bool);
+// const object = {
+//     name: 'syimyk',
+//     age: 23,
+//     isDeveloper: true
+// }
+// console.log('object', object);
+
+
+// let num = 1
+// while (num < 6) {
+//      console.log('this is workout', num);
+//      num++ 
+// }
+// console.log(end)
+
+// function toClich(str) {
+    // Разделяем строку на массив слов
+    let words = str.split(' ');
+    
+    // Объединяем слова с разделителем '!'
+    let result = words.join('!');
+    
+    return result;
+}
+
+let a = 'hip hip ohaa';
+let o = toClich(input);
+console.log(output);  
